47e62afd4e95d6eb91652b986dab128521adc2e3
tfizz <bamgbose.ab@gmail.com>
2020-10-02 16:41:33 -0400

    added routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = 3000;
 
+const routes = require("./app/routes")(app);
+
 const server = app.listen(port,()=>{
     console.log(`Server started on ${server.address().address}:${server.address().port}`);
 });
