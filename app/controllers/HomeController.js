2e9e80783ee7b38639094295bafaf92c5cd3a94c
tfizz <bamgbose.ab@gmail.com>
2020-10-02 17:12:03 -0400

    retrieve covid19 summary data from external api

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,3 +1,24 @@
-exports.default = (req,res)=>{
-    res.send("Hello World from Home Controller");
+const axios = require('axios').default;
+const COVID_DATA_API_URL = "https://api.covid19api.com/summaryx";
+
+/**
+ * This method makes a get request to fetch the summary of covid19 cases from external url
+ * @param {*} req request header
+ * @param {*} res response header
+ * @return {Object} object containing covid19 summary
+ */
+
+exports.default = async(req,res)=>{
+    try{
+        // send request to api and extract data from response
+        let {data} = await axios.get(COVID_DATA_API_URL);
+        res.send(data);
+    }
+    catch(e){
+        // error occured while making request
+        let error = e;
+        // remove stack information if present
+        if(error.stack) delete error['stack'];
+        res.send(error);
+    }
 };
